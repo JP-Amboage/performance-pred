ed675d8d7b023f54f7ed8a974ac4892e032d0768
JP-Amboage <juangamboage@gmail.com>
2022-07-25 17:31:24 +0200

    refactor

--- code/trial.py
+++ code/trial.py
@@ -1,4 +1,6 @@
 from importlib.metadata import SelectableGroups
+
+from pytest import hookspec
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
@@ -8,32 +10,32 @@ from tensorflow.keras.optimizers import SGD
 
 class Trial:
 	def __init__(self, config, X_train, X_test, y_train, y_test) -> None:
-		self.config={}
-		self.config["filters"] = config["filters"] if "filters" in config else 32
-		self.config["strides"] = config["strides"] if "strides" in config else 3
-		self.config["max_pool"] = config["max_pool"] if "max_pool" in config else 2
-		self.config["1st_dense"] = config["1st_dense"] if "1st_dense" in config else 100
-		self.config["lr"] = config["lr"] if "lr" in config else 0.01
-		self.config["momentum"] = config["momentum"] if "momentum" in config else 0.9
-
+	
+		self.config=config
 		self.X_train = X_train
 		self.X_test = X_test
 		self.y_train = y_train
 		self.y_test = y_test
-
+		self.loss = []
+		self.acc = []
 		self.model = Sequential()
-		self.model.add(Conv2D(self.config["filters"], (self.config["strides"], self.config["strides"]), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
-		self.model.add(MaxPooling2D((self.config["max_pool"], self.config["max_pool"])))
+
+		hps={}
+		hps["filters"] = config["filters"] if "filters" in config else 32
+		hps["strides"] = config["strides"] if "strides" in config else 3
+		hps["max_pool"] = config["max_pool"] if "max_pool" in config else 2
+		hps["1st_dense"] = config["1st_dense"] if "1st_dense" in config else 100
+		hps["lr"] = config["lr"] if "lr" in config else 0.01
+		hps["momentum"] = config["momentum"] if "momentum" in config else 0.9
+		self.model.add(Conv2D(hps["filters"], (hps["strides"], hps["strides"]), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+		self.model.add(MaxPooling2D((hps["max_pool"], hps["max_pool"])))
 		self.model.add(Flatten())
-		self.model.add(Dense(self.config["1st_dense"], activation='relu', kernel_initializer='he_uniform'))
+		self.model.add(Dense(hps["1st_dense"], activation='relu', kernel_initializer='he_uniform'))
 		self.model.add(Dense(10, activation='softmax'))
 		
 		# compile model
-		opt = SGD(learning_rate=self.config["lr"], momentum=self.config["momentum"])
+		opt = SGD(learning_rate=hps["lr"], momentum=hps["momentum"])
 		self.model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-
-		self.loss = []
-		self.acc = []
 	
 	def run_n_epochs(self, n: int):
 		self.model.fit(self.X_train, self.y_train, epochs=n, batch_size=32)
