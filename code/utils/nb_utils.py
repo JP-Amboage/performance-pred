e3d44131b124b8f54a68b08e90fbbc6c29c6469e
JP-Amboage <juangamboage@gmail.com>
2022-08-10 10:59:44 +0200

    refactor

--- code/utils/nb_utils.py
+++ code/utils/nb_utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import random
+from sklearn.model_selection import train_test_split
 
 #calculate finite diferences of 1st and 2nd order
 def finite_difs(curve):
@@ -39,3 +41,36 @@ def get_df_info(data_name):
 		df_info['min_curve_idx'] = 7
 	
 	return df_info
+
+#custom crossvaidation for small training set with fixed test set
+def small_train_r2_cv(model, X ,y , train_size: int, reps = 5, test_size = 0.5, rs = None):
+	if rs == None:
+		rs = random.randint(0,10000)
+
+	if X.shape[0] * (1 - test_size) < train_size:
+		train_size = int(X.shape[0] * (1 - test_size))
+		print('WARNING: not enough data for this train_size, decrease test size or train size')
+		print('\t---> Now using train size = ' + train_size)
+
+	cvs = []
+	for i in range(reps):
+		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=i)
+		X_train = X_train[:train_size]
+		y_train = y_train[:train_size]
+		model.fit(X_train,y_train.ravel())
+		cvs.append(model.score(X_test,y_test))
+
+	return np.array(cvs), [rs + i for i in range(reps)]
+
+
+def get_hps(df_info, df):
+	hps = df[df.columns[df_info['min_hp_idx']:df_info['max_hp_idx']+1]].to_numpy()
+	return hps
+
+def get_curve(df_info, known_curve, df):
+	curve = df[df.columns[df_info['min_curve_idx']:df_info['min_curve_idx']+int(df_info['num_epochs']*known_curve)]].to_numpy()
+	return curve
+	
+def get_target(df_info, df):
+	target = df[df.columns[df_info['min_curve_idx']+df_info['num_epochs']-2]].to_numpy()
+	return target
